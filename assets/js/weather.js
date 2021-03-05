aac28724a99c484177df2f4833108e8d4d51e30e
Heather Storseth <heatherstorseth3@gmail.com>
2021-03-05 09:39:00 -0500

    current weather with a high and low is displayed

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,26 +3,30 @@ $(document).ready(function() {
 
     function getApi(name) {
         let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&appid=9c15991b27b985193a8286709e2840d9&units=imperial'
-   
+//    debugger
+// console.log(name)
         fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
 
         .then(function (data) {
+            console.log(data);
             let city = $('<div>').text(data.name);
             let temp = $('<div>').text(Math.round(data.main.temp) + String.fromCharCode(176));
-            let high = $('<div>').text(data.main.temp_max);
-            let low = $('<div>').text(data.main.temp_min)
+            let high = $('<div>').text('High of ' + (data.main.temp_max) + String.fromCharCode(176));
+            let low = $('<div>').text('Low of ' + (data.main.temp_min) + String.fromCharCode(176))
 
-            $('.home').attr('style', 'border: 2px solid black')
+            // $('.home').attr('style', 'border: 2px solid black')
             $('.current-condition').append(city, temp, high, low)
         })
     }
 
     weatherButton.on('click', function() {
-        let searches = $('.text').val().trim()
+        let searches = $('.cityFinder').val().trim()
+        console.log(searches)
         getApi(searches);
+        
     })
     
 })
