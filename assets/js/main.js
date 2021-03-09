3570feae79e09176281b6b0e7f03ffea00b62133
Heather Storseth <heatherstorseth3@gmail.com>
2021-03-09 14:32:53 -0500

    modal transition search page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 // JavaScript Animations for Pageload
 
-// let tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+let tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-// tl.to('.text', {y:'0%', duration: 1, stagger: 1});
-// tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-// tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
-// tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
-// tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+tl.to('.text', {y:'0%', duration: 1, stagger: 1});
+tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
