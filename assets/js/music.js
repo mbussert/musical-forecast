153e161b00edfce914b13e4a7469606a11add9a4
Mike Bussert <mbussert@gmail.com>
2021-03-09 19:53:31 -0500

    lkajs

--- assets/js/music.js
+++ assets/js/music.js
@@ -120,8 +120,6 @@ function requestAuthorization() {
     url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
     window.location.href = url;
-
-    getPlaylist();
 }
 
 function fetchAccessToken(code) {
