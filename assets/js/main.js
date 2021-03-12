4d5367345c598681185ecd98f5d800ba19a59590
Mike Bussert <mbussert@gmail.com>
2021-03-12 13:36:43 -0500

    Final maybe?

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,4 @@ tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
 tl.fromTo('#clear-btn', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=.5');
+tl.fromTo('#inputSection', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=.25');
