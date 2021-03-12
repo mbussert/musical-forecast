8c5906ee0a9f1997014ec0cb94f4792b640ee03e
Heather Storseth <heatherstorseth3@gmail.com>
2021-03-11 19:18:13 -0500

    weather.js and others

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,29 @@
-let weatherButton = $('.button');
-let introBtn = $('#introButton');
-
 $(document).ready(function () {
-    // function recentSearches() {
 
-    // }
+    let weatherButton = $('#finder-btn');
+    let introBtn = $('#introButton');
+    recentSearches();
+
+    function recentSearches() {
+        recent = JSON.parse(localStorage.getItem('recent') || '[]');
+
+        for (let i = 0; i < recent.length; i++) {
+            let search = $('<div class="cities">').text(recent[i]);
+
+            $('#recentSearches').append(search);
+        }
+    }
+
+    $('.cities').on('click', function (event) {
+        getApi(event.target.outerText);
+    })
 
     // function that fetches api 
     function getApi(name) {
         let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&appid=9c15991b27b985193a8286709e2840d9&units=imperial'
-        
+        if (!name) {
+            return
+        }
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
@@ -23,29 +37,49 @@ $(document).ready(function () {
                 let low = $('<div>').text('Low of ' + (Math.round(data.main.temp_min) + String.fromCharCode(176)));
                 let icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
 
+                $('.current-condition').empty();
                 $('.current-condition').append(city, temp.append(icon), high, low)
+
                 getfiveday(data.coord.lat, data.coord.lon)
-            })
+
+            });
     }
 
     weatherButton.on('click', function () {
         // cityFinder is the name of the input box
-        let searches = $('.cityFinder').val()
+        let searches = $('#cityFinder').val().trim();
         // .trim()
-        console.log(searches)
+        // console.log(searches)
+        // alert()
 
         getApi(searches);
-        getfiveday();
+        // getfiveday();
+        recent = JSON.parse(localStorage.getItem('recent') || '[]');
+        if (!recent.includes(searches)) {
+            recent.push(searches)
+
+            let cities = $('<div>').text(searches);
+            $('#recent-searches').append(cities);
+        }
+        localStorage.setItem('recent', JSON.stringify(recent));
 
     });
 
     introBtn.on('click', function () {
-        $('.searchIntro').hide();
+        // $('.searchIntro').hide();
         let cSearch = $('.introInput').val().trim();
         // window.location("./assets/forecast.html")
         getApi(cSearch);
-        getfiveday();
-        // $('forecast.html').show();
+        // getfiveday();
+        // $('./assets/forecast.html').show();
+        recent = JSON.parse(localStorage.getItem('recent') || '[]');
+        if (!recent.includes(cSearch)) {
+            recent.push(cSearch)
+
+            let cities = $('<div>').text(cSearch);
+            $('#recent-searches').append(cities);
+        }
+        localStorage.setItem('recent', JSON.stringify(recent));
 
         // cSearch.hide()
         // console.log(cSearch)
@@ -54,7 +88,7 @@ $(document).ready(function () {
 
         // tl.to('.searchIntro', {y:'0%', duration: 1, stagger: 1});
 
-})
+    })
 
 
     function getfiveday(lat, lon) {
@@ -67,9 +101,16 @@ $(document).ready(function () {
 
             .then(function (data) {
                 console.log(data);
+
+                // $('#currentCard').empty();
+
                 for (let i = 1; i < 6; i++) {
+                    let day = new Date(data.daily[i].dt * 1000);
+                    let date = $('<span class="day">').text(day.toDateString());
                     let fivedaytemp = $("<div>").text(Math.round(data.daily[i].temp.day) + String.fromCharCode(176));
-                    $('.future-conditions').append(fivedaytemp)
+                    let icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.daily[i].weather[0].icon + '.png')
+                   
+                    $('.future-conditions').append(date, icon, fivedaytemp)
                 }
             })
     }
